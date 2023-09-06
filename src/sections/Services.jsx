2544a1d30994e2f94e01a1c6e4278c014c08b29a
Norchh <a.norawich@gmail.com>
2023-09-07 00:16:56 +0700

    Finished servicee section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,21 @@
+import ServiceCard from "../components/ServiceCard";
+import { services } from "../constants";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container">
+      <div className="flex justify-center flex-wrap gap-9">
+        {services.map((service) => (
+          <ServiceCard
+            key={service.label}
+            iconUrl={service.imgURL}
+            label={service.label}
+            subtext={service.subtext}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
